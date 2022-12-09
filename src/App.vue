967f458755649c91025421c03d80c2b44feb4a5c
Chanin0708 <54060999+Chanin0708@users.noreply.github.com>
2022-12-09 14:06:16 +0700

    create vue
    create vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <NavBar/>
+    <!-- <router-view /> -->
+    <Footer/>
+    
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import Footer from './components/footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    Footer
+  }
+}
+</script>
---
+++ src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <b-card no-body class="text-center">
+    <div class="bg-secondary text-light">
+      This is some content without the default <samp>&lt;b-card-body&gt;</samp> section. Notice the
+      lack of padding between the card's border and this gray <samp>&lt;div&gt;</samp>.
+    </div>
+  </b-card>
+</div>
+    </template>
+    
+    <script>
+    export default {
+      name: 'Footer',
+      props: {
+        msg: String
+      }
+    }
+    </script>
+    
